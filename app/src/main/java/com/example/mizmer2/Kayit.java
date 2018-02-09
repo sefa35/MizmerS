e462776050d256bb641b1e10e80f6b80fe7f1911
burhan35 <abogutalan35@gmail.com>
2018-02-09 03:44:24 +0300

    09.02.2018 / FIREBASE

--- app/src/main/java/com/example/mizmer2/Kayit.java
+++ app/src/main/java/com/example/mizmer2/Kayit.java
@@ -1,29 +1,49 @@
 package com.example.mizmer2;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Kayit extends AppCompatActivity {
 
+    private EditText name,email,username,pass1,pass2;
     DataBaseHelper dataBaseHelper = new DataBaseHelper(this);
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayit);
+         name = (EditText)findViewById(R.id.kayit_id_text);
+         email = (EditText)findViewById(R.id.kayit_mail_text);
+         username = (EditText)findViewById(R.id.kayit_kullanıcıadı_text);
+         pass1 = (EditText)findViewById(R.id.kayit_sifre_text);
+         pass2 = (EditText)findViewById(R.id.kayit_sifre2_text);
+
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 
-    public void onOlusturBttnClicked(View v){
 
-        EditText name = (EditText)findViewById(R.id.kayit_id_text);
-        EditText email = (EditText)findViewById(R.id.kayit_mail_text);
-        EditText username = (EditText)findViewById(R.id.kayit_kullanıcıadı_text);
-        EditText pass1 = (EditText)findViewById(R.id.kayit_sifre_text);
-        EditText pass2 = (EditText)findViewById(R.id.kayit_sifre2_text);
+
+    public void onOlusturBttnClicked(View v){
 
         String nameStr = name.getText().toString();
         String emailStr = email.getText().toString();
@@ -31,26 +51,56 @@ public class Kayit extends AppCompatActivity {
         String pass1Str = pass1.getText().toString();
         String pass2Str = pass2.getText().toString();
 
+        if (TextUtils.isEmpty(emailStr)){
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            //stopping the function execution further
+            return;
+        }
+        if (TextUtils.isEmpty(pass1Str)){
+            //password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering user..");
+        progressDialog.show();
+
         if (!pass1Str.equals(pass2Str)){   //IF CONFİRM PASS AND PASS IS NOT THE SAME
             Toast pass = Toast.makeText(Kayit.this, "Şifreler eşleşmedi!", Toast.LENGTH_SHORT);
             pass.show();
         }
         else {
-            //inser the details to database
-            Contact c = new Contact();  //creating a new object
+//            //inser the details to database
+//            Contact c = new Contact();  //creating a new object
+//
+//            //we will set constructors of database to this object
+//            c.setName(nameStr);
+//            c.setEmail(emailStr);
+//            c.setUname(usernameStr);
+//            c.setPass(pass1Str);
+//
+//            dataBaseHelper.insertContact(c);
 
-            //we will set constructors of database to this object
-            c.setName(nameStr);
-            c.setEmail(emailStr);
-            c.setUname(usernameStr);
-            c.setPass(pass1Str);
+            firebaseAuth.createUserWithEmailAndPassword(emailStr, pass1Str)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(Kayit.this,"Kayıt yapıldı", Toast.LENGTH_SHORT ).show();
+                            }else {
+                                Toast.makeText(Kayit.this,"Kayıt başarız. Tekrar deneyiniz", Toast.LENGTH_SHORT ).show();
 
-            dataBaseHelper.insertContact(c);
+                            }
+                        }
+                    });
 
-            Intent goToGiris=new Intent(this,MainActivity.class);
-            startActivity(goToGiris);
+//            startActivity(new Intent(Kayit.this,MainActivity.class));
+
+
 
         }
+
 
 
     }
